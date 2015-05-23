2523e8d27462db0e874cc43b6446b249a1554ee7
9swampy <github@prims.com>
2015-05-23 10:23:05 +0100

    Add Client.ctor overload to accept IByteStream facilitating better testing. Adding tests to cover minor TerminatedReadAsync bug fixed in 0.2.6. Minor bug fix in TerminatedReadAsync's debug output. Add overload on IByteStream.Write to accept a simple string.

---
+++ PrimS.Telnet.CiTests/WithUnconnectableClient.cs
@@ -0,0 +1,22 @@
+namespace PrimS.Telnet.CiTests
+{
+  using System;
+  using System.Threading;
+  using FakeItEasy;
+  using FluentAssertions;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class WithUnconnectableClient
+  {
+    [TestMethod]
+    public void ShouldTimeoutOnCtor()
+    {
+      IByteStream byteStream = A.Fake<IByteStream>();
+      Client sut;
+      Action act = () => sut = new Client(byteStream, default(CancellationToken), new TimeSpan(0, 0, 0, 0, 1));
+
+      act.ShouldThrow<InvalidOperationException>().WithMessage("Unable to connect to the host.");
+    }
+  }
+}
