e86d436cb30261e475d8ddc365aab925cd0348d2
A9G-Data-Droid <akauffman@cmm-measure.com>
2018-05-10 09:56:16 -0700

    Added all Options and Commands. Added handlers for non printable TELNET messages. Added Cancel when Interrupt is recieved. Stopped responding to DONT and WONT. Added Subnegotiation for Terminal Type. Changed Terminator detection to Contains.

--- PrimS.Telnet.NetStandard/Commands.cs
+++ PrimS.Telnet.NetStandard/Commands.cs
@@ -3,6 +3,17 @@
   [IsNotDeadCodeAttribute]
   internal enum Commands
   {
+    SubnegotiationEnd = 240,
+    NoOperation = 241,
+    Data = 242,
+    Break = 243,
+    InterruptProcess = 244,
+    AbortOutput = 245,
+    AreYouThere = 246,
+    EraseCharacter = 247,
+    EraseLine = 248,
+    GoAhead = 249,
+    Subnegotiation = 250,
     Will = 251,
     Wont = 252,
     Do = 253,
--- PrimS.Telnet.NetStandard/Options.cs
+++ PrimS.Telnet.NetStandard/Options.cs
@@ -1,8 +1,19 @@
-namespace PrimS.Telnet
+﻿namespace PrimS.Telnet
 {
   [IsNotDeadCodeAttribute]
   internal enum Options
   {
-    SuppressGoAhead = 3
+    Echo = 1,
+    SuppressGoAhead = 3,
+    Status = 5,
+    TimingMark = 6,
+    TerminalType = 24,
+    WindowSize = 31,
+    TerminalSpeed = 32,
+    RemoteFlowCcontrol = 33,
+    LineMode = 34,
+    OldEnvironment = 36,
+    NewEnvironment = 39,
+    CharacterSet = 42
   }
 }
